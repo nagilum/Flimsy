4e2181413db7d8f7ff74cc4bb47f64842113b58b
Stian Hanger <pdnagilum@gmail.com>
2018-09-10 15:31:59 +0200

    Expand the config class

--- FlimsyConfig.cs
+++ FlimsyConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 
@@ -7,6 +8,14 @@ namespace Flimsy {
         #region Properties
 
         public bool Debug { get; set; }
+
+        public bool UseProdDatabase { get; set; }
+
+        public FlimsyConfigDatabase TestDatabase { get; set; }
+
+        public FlimsyConfigDatabase ProdDatabase { get; set; }
+
+        public Dictionary<string, string> Settings { get; set; }
 
         #endregion
 
@@ -31,6 +40,42 @@ namespace Flimsy {
 
             return JsonConvert.DeserializeObject<FlimsyConfig>(
                 File.ReadAllText(file));
+        }
+
+        #endregion
+
+        #region Helper classes
+
+        public class FlimsyConfigDatabase {
+            public string Username { get; set; }
+
+            public string Password { get; set; }
+
+            public string Hostname { get; set; }
+
+            public string Database { get; set; }
+
+            public int? ConnectTimeout { get; set; }
+
+            public int? MultipleActiveResultSets { get; set; }
+
+            /// <summary>
+            /// Get a compiled connection string.
+            /// </summary>
+            public override string ToString() {
+                return string.Format(
+                    "Data Source={0};Initial Catalog={1};User ID={2};Password={3};{4}{5}",
+                    this.Hostname,
+                    this.Database,
+                    this.Username,
+                    this.Password,
+                    this.ConnectTimeout.HasValue
+                        ? "Connect Timeout=" + this.ConnectTimeout.Value + ";"
+                        : null,
+                    this.MultipleActiveResultSets.HasValue
+                        ? "MultipleActiveResultSets=" + this.MultipleActiveResultSets.Value + ";"
+                        : null);
+            }
         }
 
         #endregion
